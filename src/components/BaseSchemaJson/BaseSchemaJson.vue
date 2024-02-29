acc2182a0d069d71015e5d24b9e30d609f2c84ab
nelyaklyusa <nelya.klyusa@gmail.com>
2024-02-29 11:28:40 +0200

    add schema

---
+++ src/components/BaseSchemaJson/BaseSchemaJson.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { onMounted } from 'vue';
+
+const props = defineProps<{
+  data: [object, any[]];
+}>();
+
+onMounted(() => {
+  const script = document.createElement('script');
+  script.type = 'application/ld+json';
+  script.innerHTML = JSON.stringify(props.data);
+  document.body.appendChild(script);
+});
+</script>
