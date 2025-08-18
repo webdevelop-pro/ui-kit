669c083d15776f296a28b5e5c44fbc4de93fa887
nelyaklyusa <nelya.klyusa@gmail.com>
2025-08-18 20:40:09 +0300

    update card offer

--- src/components/VComment/VCommentItems.vue
+++ src/components/VComment/VCommentItems.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 import { PropType } from 'vue';
 import TheCommentThread, { IOfferComment } from 'UiKit/components/VComment/VCommentThread.vue';
+import VSkeleton from 'UiKit/components/Base/VSkeleton/VSkeleton.vue';
 
 defineProps({
   comments: {
     type: Array as PropType<IOfferComment[]>,
     required: true,
+  },
+  loading: {
+    type: Boolean,
+    default: false,
   },
 });
 </script>
@@ -15,12 +20,20 @@ defineProps({
     <div class="v-comment-items__title is--h2__title">
       <slot>Questions</slot>
     </div>
-    <TheCommentThread
-      v-for="item in comments"
-      :key="item.created_at"
-      :comment="item"
-      class="v-comment-items__thread"
+    <VSkeleton
+      v-if="loading"
+      height="22px"
+      width="100%"
+      class="v-comment-items__skeleton"
     />
+    <template v-else>
+      <TheCommentThread
+        v-for="item in comments"
+        :key="item.created_at"
+        :comment="item"
+        class="v-comment-items__thread"
+      />
+    </template>
   </div>
 </template>
 
