674152deb151c0d0b8f75fa8038c169fdaeae75a
nelyaklyusa <nelya.klyusa@gmail.com>
2025-11-03 15:48:50 +0200

    fiix lint

--- src/markdown/markdown-grid.ts
+++ src/markdown/markdown-grid.ts
@@ -2,18 +2,16 @@ import type MarkdownIt from 'markdown-it';
 import type Token from 'markdown-it/lib/token.mjs';
 import type { RenderRule } from 'markdown-it/lib/renderer.mjs';
 import container from 'markdown-it-container';
-import { IFrontmatter } from 'UiKit/types/types';
 
 
 type ContainerArgs = [typeof container, string, { render: RenderRule }];
 const createContainer = (
   klass: string,
-  md: MarkdownIt,
 ): ContainerArgs => [
   container,
   klass,
   {
-    render(tokens: Token[], idx: number, options: unknown, env: { frontmatter: IFrontmatter }) {
+    render(tokens: Token[], idx: number) {
       const token = tokens[idx];
       if (token.nesting === 1) {
         return (
